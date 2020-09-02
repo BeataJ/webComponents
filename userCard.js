a4dacd2a332fc79b3a19f9d5bc7dd9196a3c8149
beata <beata@emc2wave.com>
2020-09-02 15:40:17 -0700

    add Shodow

--- userCard.js
+++ userCard.js
@@ -1,6 +1,10 @@
 class UserCard extends HTMLElement {
   constructor() {
     super();
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+
     this.innerHTML = `<style>h3 {color: coral}</style><h3>${this.getAttribute(
       'name'
     )}<\h3>`;
